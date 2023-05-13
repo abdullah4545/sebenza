56b81a168aa8ac1f6ad71ceb4fb0e6e88a7bbd58
abdullah4545 <md.muraiem@gmail.com>
2023-05-14 01:04:01 +0600

    d

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use QueryException;
 
 class Handler extends ExceptionHandler
 {
@@ -35,7 +34,7 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (QueryException $e,$request) {
+        $this->reportable(function (\Illuminate\Database\QueryException $e,$request) {
             if($request->is('api/*')){
                 $response=[
                     "status"=>false,
