98480c6be20c817d656f2472b6e7f2028e98bda8
abdullah4545 <md.muraiem@gmail.com>
2023-05-14 01:15:02 +0600

    d

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -35,15 +35,13 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (QueryException $e, $request) {
-            if($request->is('api/*')){
+        $this->reportable(function (QueryException  $request) {
+
                 $response=[
                     "status"=>false,
                     'message' => "No query found for this url",
                 ];
                 return response()->json($response, 200);
-            }
         });
     }
 }
-
