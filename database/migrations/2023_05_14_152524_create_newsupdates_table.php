381171f8f92276a560d0d3997e94b94976674a45
abdullah4545 <md.muraiem@gmail.com>
2023-05-21 08:12:49 +0600

    f

--- database/migrations/2023_05_14_152524_create_newsupdates_table.php
+++ database/migrations/2023_05_14_152524_create_newsupdates_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text('title');
             $table->string('slug');
             $table->longText('news');
-            $table->boolean('seen');
+            $table->boolean('seen')->nullable();
             $table->text('postImage')->nullable();
             $table->string('status')->default('Active');
             $table->timestamps();
